0fc5240c6b722b18b3dfe61eb3eb6d163c9d0da8
highrow623 <danieltmetro@gmail.com>
2025-05-25 13:59:47 -0600

    preliminary time trial logic

--- AP_Randomizer/apworld/rules_normal.py
+++ AP_Randomizer/apworld/rules_normal.py
@@ -442,16 +442,30 @@ class PseudoregaliaNormalRules(PseudoregaliaRulesHelpers):
                     self.can_bounce(state)
                     or self.get_kicks(state, 2))
                 or self.can_slidejump(state) and self.has_gem(state) and self.get_kicks(state, 1),
-            
+
             # TODO (time-trials): finish logic
-            "Dilapidated Dungeon - Time Trial": lambda state: True,
-            "Castle Sansa - Time Trial": lambda state: True,
-            "Sansa Keep - Time Trial": lambda state: True,
-            "Listless Library - Time Trial": lambda state: True,
-            "Twilight Theatre - Time Trial": lambda state: True,
-            "Empty Bailey - Time Trial": lambda state: True,
-            "The Underbelly - Time Trial": lambda state: True,
-            "Tower Remains - Time Trial": lambda state: True,
+            "Dilapidated Dungeon - Time Trial": lambda state:
+                self.has_breaker(state) and self.has_plunge(state) and self.get_kicks(state, 3)
+                and self.has_gem(state) and self.can_slidejump(state) and self.can_bounce(state),
+            "Castle Sansa - Time Trial": lambda state:
+                self.has_small_keys(state),
+            "Sansa Keep - Time Trial": lambda state:
+                self.has_breaker(state) and self.has_plunge(state) and self.get_kicks(state, 3)
+                and self.has_gem(state) and self.can_slidejump(state) and self.can_bounce(state),
+            "Listless Library - Time Trial": lambda state:
+                self.has_breaker(state) and self.has_plunge(state) and self.has_gem(state),
+            "Twilight Theatre - Time Trial": lambda state:
+                self.has_breaker(state) and self.has_plunge(state) and self.get_kicks(state, 3)
+                and self.has_gem(state) and self.can_slidejump(state) and self.can_bounce(state),
+            "Empty Bailey - Time Trial": lambda state:
+                self.has_breaker(state) and self.has_plunge(state) and self.get_kicks(state, 3)
+                and self.has_gem(state) and self.can_slidejump(state) and self.can_bounce(state),
+            "The Underbelly - Time Trial": lambda state:
+                self.has_breaker(state) and self.has_plunge(state) and self.get_kicks(state, 3)
+                and self.has_gem(state) and self.can_slidejump(state) and self.can_bounce(state),
+            "Tower Remains - Time Trial": lambda state:
+                self.has_breaker(state) and self.has_plunge(state) and self.get_kicks(state, 3)
+                and self.has_gem(state) and self.can_slidejump(state) and self.can_bounce(state),
         }
 
         self.apply_clauses(region_clauses, location_clauses)

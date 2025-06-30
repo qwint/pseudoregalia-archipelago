08aefc9653ceca5107a4dc0e6a5582474205ae09
highrow623 <danieltmetro@gmail.com>
2025-06-29 21:08:47 -0600

    reformat settings load so only parse is in the try catch block

--- AP_Randomizer/src/Settings.cpp
+++ AP_Randomizer/src/Settings.cpp
@@ -32,71 +32,73 @@ namespace Settings {
 			}
 		}
 
+		toml::table settings_table;
 		try {
-			toml::table settings_table = toml::parse(settings_file);
-			Log("Loading settings");
-
-			// item_display
-			optional<string> item_display_setting = settings_table["settings"]["item_display"].value<string>();
-			if (item_display_setting) {
-				string setting_value = item_display_setting.value();
-				if (setting_value == "full") {
-					item_display = ItemDisplay::Full;
-					Log("item_display set to " + setting_value);
-				}
-				else if (setting_value == "generic_non_pseudo") {
-					item_display = ItemDisplay::GenericNonPseudo;
-					Log("item_display set to " + setting_value);
-				}
-				else if (setting_value == "generic_all") {
-					item_display = ItemDisplay::GenericAll;
-					Log("item_display set to " + setting_value);
-				}
-				else {
-					Log("Unknown option " + setting_value + " for item_display, using default option full");
-				}
-			}
-			else {
-				Log("Using default option full for item_display");
-			}
-
-			// popups
-			optional<string> popups_setting = settings_table["settings"]["popups"].value<string>();
-			if (popups_setting) {
-				string setting_value = popups_setting.value();
-				if (setting_value == "show_with_sound") {
-					popups = Popups::ShowWithSound;
-					Log("popups set to " + setting_value);
-				}
-				else if (setting_value == "show_muted") {
-					popups = Popups::ShowMuted;
-					Log("popups set to " + setting_value);
-				}
-				else if (setting_value == "hide") {
-					popups = Popups::Hide;
-					Log("popups set to " + setting_value);
-				}
-				else {
-					Log("Unknown option " + setting_value + " for popups, using default option show_with_sound");
-				}
-			}
-			else {
-				Log("Using default option show_with_sound for popups");
-			}
-
-			// death_link
-			optional<bool> death_link_setting = settings_table["settings"]["death_link"].value<bool>();
-			if (death_link_setting) {
-				death_link = death_link_setting.value();
-				string setting_string = death_link_setting.value() ? "true" : "false";
-				Log("death_link set to " + setting_string);
-			}
-			else {
-				Log("Using default option false for death_link");
-			}
+			settings_table = toml::parse(settings_file);
 		}
 		catch (const toml::parse_error& err) {
 			Log("Failed to parse settings: " + string(err.description()));
+			return;
+		}
+		Log("Loading settings");
+
+		// item_display
+		optional<string> item_display_setting = settings_table["settings"]["item_display"].value<string>();
+		if (item_display_setting) {
+			string setting_value = item_display_setting.value();
+			if (setting_value == "full") {
+				item_display = ItemDisplay::Full;
+				Log("item_display set to " + setting_value);
+			}
+			else if (setting_value == "generic_non_pseudo") {
+				item_display = ItemDisplay::GenericNonPseudo;
+				Log("item_display set to " + setting_value);
+			}
+			else if (setting_value == "generic_all") {
+				item_display = ItemDisplay::GenericAll;
+				Log("item_display set to " + setting_value);
+			}
+			else {
+				Log("Unknown option " + setting_value + " for item_display, using default option full");
+			}
+		}
+		else {
+			Log("Using default option full for item_display");
+		}
+
+		// popups
+		optional<string> popups_setting = settings_table["settings"]["popups"].value<string>();
+		if (popups_setting) {
+			string setting_value = popups_setting.value();
+			if (setting_value == "show_with_sound") {
+				popups = Popups::ShowWithSound;
+				Log("popups set to " + setting_value);
+			}
+			else if (setting_value == "show_muted") {
+				popups = Popups::ShowMuted;
+				Log("popups set to " + setting_value);
+			}
+			else if (setting_value == "hide") {
+				popups = Popups::Hide;
+				Log("popups set to " + setting_value);
+			}
+			else {
+				Log("Unknown option " + setting_value + " for popups, using default option show_with_sound");
+			}
+		}
+		else {
+			Log("Using default option show_with_sound for popups");
+		}
+
+		// death_link
+		optional<bool> death_link_setting = settings_table["settings"]["death_link"].value<bool>();
+		if (death_link_setting) {
+			death_link = death_link_setting.value();
+			string setting_string = death_link_setting.value() ? "true" : "false";
+			Log("death_link set to " + setting_string);
+		}
+		else {
+			Log("Using default option false for death_link");
 		}
 	}
 

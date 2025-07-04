484f70e58c26a1aca0ac873c2f7f92ae6a09655b
highrow623 <danieltmetro@gmail.com>
2025-07-04 15:49:32 -0600

    refactor ParseSetting to remove setting default multiple times

--- AP_Randomizer/src/Settings.cpp
+++ AP_Randomizer/src/Settings.cpp
@@ -21,14 +21,15 @@ namespace Settings {
 		// if you run from the game directory
 		const string settings_filename2 = "pseudoregalia/Binaries/Win64/Mods/AP_Randomizer/settings.toml";
 
+		// settings, set to their defaults
 		ItemDisplay item_display = ItemDisplay::Full;
 		InteractableAuraDisplay interactable_aura_display = InteractableAuraDisplay::Classification;
 		bool death_link = false;
 		PopupsInitialState popups_initial_state = PopupsInitialState::ShowWithSound;
 		bool popups_simplify_item_font = false;
 
-		template<class E> E ParseSetting(toml::table, string, unordered_map<string, E>, string);
-		bool ParseSetting(toml::table, string, bool);
+		template<class E> void ParseSetting(E&, toml::table, string, unordered_map<string, E>);
+		void ParseSetting(bool&, toml::table, string);
 	}
 
 	void Load() {
@@ -52,35 +53,35 @@ namespace Settings {
 		}
 
 		Log("Loading settings");
-		item_display = ParseSetting(
+		ParseSetting(
+			item_display,
 			settings_table,
 			"settings.item_display",
 			unordered_map<string, ItemDisplay>{
 				{ "full", ItemDisplay::Full },
 				{ "generic_non_pseudo", ItemDisplay::GenericNonPseudo },
 				{ "generic_all", ItemDisplay::GenericAll },
-			},
-			"full");
-		interactable_aura_display = ParseSetting(
+			});
+		ParseSetting(
+			interactable_aura_display,
 			settings_table,
 			"settings.interactable_aura_display",
 			unordered_map<string, InteractableAuraDisplay>{
 				{ "classification", InteractableAuraDisplay::Classification },
 				{ "generic", InteractableAuraDisplay::Generic },
 				{ "none", InteractableAuraDisplay::None },
-			},
-			"classification");
-		death_link = ParseSetting(settings_table, "settings.death_link", false);
-		popups_initial_state = ParseSetting(
+			});
+		ParseSetting(death_link, settings_table, "settings.death_link");
+		ParseSetting(
+			popups_initial_state,
 			settings_table,
 			"settings.popups.initial_state",
 			unordered_map<string, PopupsInitialState>{
 				{ "show_with_sound", PopupsInitialState::ShowWithSound },
 				{ "show_muted", PopupsInitialState::ShowMuted },
 				{ "hide", PopupsInitialState::Hide },
-			},
-			"show_with_sound");
-		popups_simplify_item_font = ParseSetting(settings_table, "settings.popups.simplify_item_font", false);
+			});
+		ParseSetting(popups_simplify_item_font, settings_table, "settings.popups.simplify_item_font");
 	}
 
 	ItemDisplay GetItemDisplay() {
@@ -104,33 +105,37 @@ namespace Settings {
 	}
 
 	namespace {
-		template<class E> E ParseSetting(toml::table settings_table, string setting_path, unordered_map<string, E> option_map, string default_option) {
+		template<class E> void ParseSetting(
+			E& setting,
+			toml::table settings_table,
+			string setting_path,
+			unordered_map<string, E> option_map
+		) {
 			optional<string> option = settings_table.at_path(setting_path).value<string>();
 			if (option) {
 				if (option_map.contains(*option)) {
 					Log(setting_path + " = " + *option);
-					return option_map.at(*option);
+					setting = option_map.at(*option);
+					return;
 				}
 
-				Log(setting_path + " = " + default_option + " (using default, unknown option " + *option + ")");
-				return option_map.at(default_option);
+				Log(setting_path + " = default (unknown option " + *option + ")");
+				return;
 			}
 
-			Log(setting_path + " = " + default_option + " (using default, setting missing or not a string)");
-			return option_map.at(default_option);
+			Log(setting_path + " = default (setting missing or not a string)");
+			return;
 		}
 
-		bool ParseSetting(toml::table settings_table, string setting_path, bool default_option) {
+		void ParseSetting(bool& setting, toml::table settings_table, string setting_path) {
 			optional<bool> option = settings_table.at_path(setting_path).value<bool>();
 			if (option) {
 				string option_string = *option ? "true" : "false";
 				Log(setting_path + " = " + option_string);
-				return *option;
+				setting = *option;
+				return;
 			}
-
-			string default_option_string = default_option ? "true" : "false";
-			Log(setting_path + " = " + default_option_string + " (using default, setting missing or not a bool)");
-			return default_option;
+			Log(setting_path + " = default (setting missing or not a bool)");
 		}
 	}
 }
